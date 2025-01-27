5a9cc792a7984f705b9ec396bb507797fa66edcc
Nakildias <130524745+Nakildias@users.noreply.github.com>
2025-01-27 16:59:22 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Checking dependencies
+sudo pacman -Sy bc lshw
+echo "Making bashfetch executable"
+chmod +x ./bashfetch
+echo "Moving bashfetch to /bin/bashfetch"
+echo "Root privilege is required to write into /bin"
+sudo mv ./bashfetch /bin/
+bashfetch
+echo "You should now be able to use the command "bashfetch""
