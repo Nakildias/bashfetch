c12ed3d1133e04469df515ff6276d7b3cbf1696d
Nakildias <130524745+Nakildias@users.noreply.github.com>
2025-03-20 00:06:54 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,41 @@
 #!/bin/bash
 #Checking dependencies
-sudo pacman -Sy bc lshw dmidecode
+#DEPENDENCIES
+ #ARCH LINUX
+  sudo pacman -Sy bc lshw dmidecode
+ #DEBIAN
+  #WIP
+ #FEDORA
+  #WIP
+ #GENTOO
+  #WIP
 echo "Making bashfetch executable"
 chmod +x ./bashfetch
 echo "Moving bashfetch to /bin/bashfetch"
 echo "Root privilege is required to write into /bin"
 sudo mv ./bashfetch /bin/
-bashfetch
+
+# bashfetch path
+FILE="/bin/bashfetch"
+
+while true; do
+    # Ask the user
+    read -p "Are you using a virtual machine? (y/n): " response
+    
+    # Convert response to lowercase
+    response=$(echo "$response" | tr '[:upper:]' '[:lower:]')
+    
+    if [[ "$response" == "y" ]]; then
+        # Use sed to modify line #9
+        sudo sed -i '9s/VM=False/VM=True/' "$FILE"
+        echo "VM setting updated in $FILE."
+        break
+    elif [[ "$response" == "n" ]]; then
+        echo "VM setting was unchanged in $FILE."
+        break
+    else
+        echo "Invalid response. Please enter 'y' or 'n'."
+    fi
+done
+echo ""
 echo "You should now be able to use the command "bashfetch""
