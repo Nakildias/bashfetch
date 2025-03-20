bc63e189b3c314d190b72b703eab66d4fd0018c7
Nakildias <130524745+Nakildias@users.noreply.github.com>
2025-03-20 13:27:04 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ if command -v dnf &> /dev/null; then
 fi
  #GENTOO
 if command -v emerge &> /dev/null; then
-  sudo emerge --sync && sudo emerge --ask sys-apps/bc sys-apps/lshw sys-apps/dmidecode
+  sudo emerge --sync && sudo emerge --ask sys-devel/bc sys-apps/lshw sys-apps/dmidecode
 fi
 
 echo "Making bashfetch executable"
